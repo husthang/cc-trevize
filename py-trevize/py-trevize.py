a595b9be48ec2f3fa0f8cddc066a7ffc2d5cd41c
Frank Hu <mofrankhu@gmail.com>
2016-03-06 07:45:26 +0800

    py-trevize 0.11: solve multigraph input

--- py-trevize/py-trevize.py
+++ py-trevize/py-trevize.py
@@ -16,22 +16,32 @@ def read_csv(g, stv1):
 
     input: two csv input file
         detail of input format in http://codecraft.huawei.com/home/detail
+        solve multigraph in input
     output: NetworkX-graph
     """
     
     import re
     num = re.compile('\d+')
     
-    DG = nx.DiGraph()
+    G = nx.DiGraph()
     for line in g:
         nums = re.findall(num, line)
 
         for i in range(4):
             nums[i] = int(nums[i])  # re str to int
-        DG.add_edges_from([(nums[1], nums[2])], weight=nums[3], label=nums[0])
-    
+
+        # solve multigraph: find best edge and apply
+        try:
+            exist_weight = G[nums[1]][nums[2]]['weight']
+        except KeyError:
+            G.add_edge(nums[1], nums[2], weight=nums[3], label=nums[0])
+        else:
+            if exist_weight > nums[3]:  # need update
+                G[nums[1]][nums[2]]['weight'] = nums[3]
+                G[nums[1]][nums[2]]['label'] = nums[0]
+
+    # s, t, v1(may be blank)
     for line in stv1:
-        # s, t, v1(may be blank)
         stv1_list = re.findall(num, line)
         for i in range(len(stv1_list)):
             stv1_list[i] = int(stv1_list[i])
@@ -39,16 +49,18 @@ def read_csv(g, stv1):
         t = stv1_list[1]
         v1 = stv1_list[2:]
     
-    return DG, s, t, v1
+    return G, s, t, v1
 
 
-def check_input(DG, s, t, v1):
-    print('nodes:', DG.nodes())
-    print('edges:', DG.edges())
+def check_input(G, s, t, v1):
+    print('nodes:', G.nodes())
+    from pprint import pprint
+    print('edges:')
+    pprint(G.edges(data=True))
 
-    layout=nx.circular_layout(DG)
-    nx.draw_networkx(DG,pos=layout)
-    nx.draw_networkx_edge_labels(DG, pos=layout, label_pos=0.5, font_size=8)
+    layout=nx.circular_layout(G)
+    nx.draw_networkx(G,pos=layout)
+    nx.draw_networkx_edge_labels(G, pos=layout, label_pos=0.5, font_size=8)
 
     print('s: {}, t: {}'.format(s, t))
     print('v1', v1)
@@ -56,19 +68,19 @@ def check_input(DG, s, t, v1):
     plt.show()
 
 
-def trevize(DG, s, t, v1):
+def trevize(G, s, t, v1):
     """Find best path.
 
     input: graph, s, t, v1
     output: best path
     """
-    all_paths = list(nx.shortest_simple_paths(DG, s, t))
+    all_paths = list(nx.shortest_simple_paths(G, s, t))
     set_v1 = set(v1)
     for path in all_paths:
         print(path)
         if set_v1 <= set(path[1:-1]):
             print('find path')
-            return path, DG
+            return path, G
     print('no path')
     return "NA"
 
@@ -85,23 +97,24 @@ def write_csv(f, answer):
         f.write(answer)
         return 0
     # else
-    path, DG = answer
-    print(DG.edges())
+    path, G = answer
+    print(G.edges())
     answer = ""
     for i in range(len(path) - 1):
         # as used i+1
-        print(DG[path[i]][path[i+1]]['label'])
-        answer += str(DG.edge[path[i]][path[i+1]]['label']) + '|'
+        print(G[path[i]][path[i+1]]['label'])
+        answer += str(G.edge[path[i]][path[i+1]]['label']) + '|'
     print(answer[:-1])
     f.write(answer[:-1])
     return 0
 
 
 def main():
-    DG, s, t, v1 = read_csv(g,stv1)
-    #check_input(DG, s, t, v1)
-    answer = trevize(DG, s, t, v1)
+    G, s, t, v1 = read_csv(g,stv1)
+    #check_input(G, s, t, v1)
+    answer = trevize(G, s, t, v1)
     write_csv(o, answer)
+
 
 if __name__ == '__main__':
     main()
