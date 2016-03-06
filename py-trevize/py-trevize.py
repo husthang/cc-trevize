afeb3697422ceeca7dbe83b5bd015851fec6ce88
Frank Hu <mofrankhu@gmail.com>
2016-03-06 08:05:25 +0800

    py-trevize 0.12: bug fix and minor improvemtents.
    - fix bug of not using weight in finding path
    - add verbose option for trevize() and write_csv()
    - add argv support for file i/o
    - add some comments

--- py-trevize/py-trevize.py
+++ py-trevize/py-trevize.py
@@ -5,11 +5,8 @@
 # Enviroment: Python 3, NetworkX
 
 import networkx as nx
-import matplotlib.pyplot as plt
+from sys import argv
 
-g = open('../test/g.csv')
-stv1 = open('../test/stv1.csv')
-o = open('../test/o.csv', 'w')
 
 def read_csv(g, stv1):
     """Read the two csv input files.
@@ -19,7 +16,6 @@ def read_csv(g, stv1):
         solve multigraph in input
     output: NetworkX-graph
     """
-    
     import re
     num = re.compile('\d+')
     
@@ -53,11 +49,15 @@ def read_csv(g, stv1):
 
 
 def check_input(G, s, t, v1):
-    print('nodes:', G.nodes())
+    """Print input data."""
     from pprint import pprint
+    import matplotlib.pyplot as plt
+
+    print('nodes:', G.nodes())
     print('edges:')
     pprint(G.edges(data=True))
 
+    # draw graph and edge labels in same layout
     layout=nx.circular_layout(G)
     nx.draw_networkx(G,pos=layout)
     nx.draw_networkx_edge_labels(G, pos=layout, label_pos=0.5, font_size=8)
@@ -68,29 +68,33 @@ def check_input(G, s, t, v1):
     plt.show()
 
 
-def trevize(G, s, t, v1):
+def trevize(G, s, t, v1, verbose=False):
     """Find best path.
 
     input: graph, s, t, v1
     output: best path
     """
-    all_paths = list(nx.shortest_simple_paths(G, s, t))
+    all_paths = list(nx.shortest_simple_paths(G, s, t, weight='weight'))
     set_v1 = set(v1)
     for path in all_paths:
-        print(path)
+        if verbose:
+            print(path)
         if set_v1 <= set(path[1:-1]):
-            print('find path')
+            # use set to find if all elements in v' are in path
+            if verbose:
+                print('find path')
             return path, G
-    print('no path')
+    if verbose:
+        print('no path')
     return "NA"
 
 
-def write_csv(f, answer):
+def write_csv(f, answer, verbose=False):
     """Write the answer to csv file.
 
-    format: 
-        - "NA" for no answer; 
-        - e[1]|e[2]|..|e[n] for shortest path, e[i] is label of edge
+    proposed format: 
+        - 'NA' for no answer; 
+        - 'e[1]|e[2]|..|e[n]' for shortest path, e[i] is label of edge
     """
     if answer is "NA":
         print(answer)
@@ -98,11 +102,11 @@ def write_csv(f, answer):
         return 0
     # else
     path, G = answer
-    print(G.edges())
     answer = ""
     for i in range(len(path) - 1):
         # as used i+1
-        print(G[path[i]][path[i+1]]['label'])
+        if verbose:
+            print(G[path[i]][path[i+1]]['label'])
         answer += str(G.edge[path[i]][path[i+1]]['label']) + '|'
     print(answer[:-1])
     f.write(answer[:-1])
@@ -110,10 +114,19 @@ def write_csv(f, answer):
 
 
 def main():
+    # file i/o:
+    # g.csv: graph data; stv1.csv: path data
+    # o.csv: file to write
+    script, g, stv1, o = argv
+    g = open(g)
+    stv1 = open(stv1)
+    o = open(o, 'w')
+
     G, s, t, v1 = read_csv(g,stv1)
-    #check_input(G, s, t, v1)
-    answer = trevize(G, s, t, v1)
-    write_csv(o, answer)
+    # check_input(G, s, t, v1)
+    # set verbose to True to get more output infomation
+    answer = trevize(G, s, t, v1, verbose=False)
+    write_csv(o, answer, verbose=False)
 
 
 if __name__ == '__main__':
