18ffc57187f844cebe52e344ce93eac4c5822fd0
Frank Hu <humour114@gmail.com>
2016-03-09 23:52:38 +0800

    Gaia, the test framework for Trevize.

---
+++ gaia/gaia.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+"""Gaia: the test framework for Trevize
+
+- input:
+    - algorithms
+        - should meet run format of `algorithm topo.csv demand.csv out.csv`
+        - note: algorithm may be single algorithm,
+                or hybrid of different algorithms.
+    - test cases
+- process:
+    - for each case, test every algorithm
+- output: to a log file
+    - features of the test case
+    - result of algorithms
+        - time
+        - route
+        - weight
+
+"""
+
+# Author: Mo Frank Hu (mofrankhu@gmail.com)
+# Dependency: Python 3
+
+
+import argparse
+
+
+def parse_input():
+    pass
+
+def process():
+    pass
+
+def output():
+    pass
+
+
+if __name__ == '__main__':
+    parse_input()
+    process()
+    output()
