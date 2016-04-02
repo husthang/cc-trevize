65bf51cdbe4fa1fabb4143a70dc8aaa739a440e1
Mo Frank Hu <mofrankhu@gmail.com>
2016-04-02 12:53:31 +0800

    trevize 1.2.0: minor updates.
    - add comments.
    - new merge_dict() by new expression in py 3.5.
    Note in #14

--- trevize/trevize.py
+++ trevize/trevize.py
@@ -16,7 +16,7 @@
 # Dependencies: Python 3, NetworkX
 
 import networkx as nx
-import time
+import time  # get benchmark data
 
 
 def read_csv(g, stv1):
@@ -84,13 +84,23 @@ def trevize(G, s, t, v1, verbose):
 
     paths = deque()  # use as stack for DFS
     set_v1 = set(v1)
+    dict_v1 = {}
+    for i in v1:
+        dict_v1[i] = True
     valid_paths = []
 
-    # Y, N dict for edges
+    # Y, N dict for edge pair DP
+    # format: edge1: {(edge, '')}
     Y = {}
-    Y_inv = {}  # inverse dict of Y
     N = {}
-    N_global = {}
+
+    # global wrong edges list
+    N_global = {}  # format: {edge: ''}
+
+    # found path for DP
+    # vertex:value dict
+    found_path = {}
+    cycle = {}
 
     global num_paths, max_weight
     num_paths = 0
@@ -128,7 +138,7 @@ def trevize(G, s, t, v1, verbose):
             - next vertices:
                 - outdegree == 0: dead end
                 - outdegree == 1: add to Y_list
-            """
+        """
         global num_paths, max_weight, i_searched
 
         path, weight, Y_path, N_path = paths.popleft()
@@ -138,51 +148,99 @@ def trevize(G, s, t, v1, verbose):
         if end_vertex is not s:
             # must have >= 1 edge
             end_edge = (path[-2], path[-1])
-            Y[end_edge] = {}
+            if end_edge not in Y:
+                Y[end_edge] = {}
+            if end_edge not in N:
+                N[end_edge] = {}
 
         next_v_list = G[end_vertex]
         if len(next_v_list) is 0:  # dead end
             # del edge and end vertex
             G.remove_node(end_vertex)
-        elif len(next_v_list) is 1:  # one outdegree
+            if len(path) >= 2:
+                G.remove_edge((path[-2],path[-1]))
+        elif len(next_v_list) is 1:
+            # only one outdegree: add to Y-pair list
             # print(next_v_list)
             for key in next_v_list:
                 next_v = key
-            next_edge = (end_vertex, next_v)
+            next_edge = (end_vertex, next_v)  # the only out-edge
+            if next_edge in N_global:
+            # this vertex will always lead to the wrong end, delete it.
+                G.remove_node(end_vertex)
+                return
             if end_vertex is not s:
                 if next_edge in N[end_edge]:  # forming cycle
+                    add_cycle(path + [end_vertex], cycle)
                     return
                 if next_edge not in Y[end_edge]:
+                    # end_edge -> next_edge
+                    # merge N_edge
+                    N[end_edge] = merge_dicts(N[end_edge], N[next_edge])
                     # adding edge to Y list (by extending inverse list)
                     Y[end_edge][next_edge] = ""
-                    N[end_edge] = merge_dicts(N[end_edge], N[next_edge])
-                    Y_inv[next_edge] = {}
-                    Y_inv[next_edge][end_edge] = ""
-                    if end_edge in Y_inv:
-                        for edge in Y_inv[end_edge]:
-                            Y_inv[next_edge][edge] = ""
-                            Y[edge][next_edge] = ""
-                            if next_edge in N[edge]:
-                                # edge that must lead to WA
-                                G.remove_edge(edge[0], edge[1])
-                                N_global[edge] = ""
+                    if next_edge in Y:
+                        Y[end_edge] = merge_dicts(Y[end_edge], Y[next_edge])
+                        for e in Y[next_edge]:
+                        # path must extend to these edges
+                            if e in N[end_edge]:
+                            # end edge will always lead to wrong
+                                print("del end edge in searching.")
+                                G.remove_edge(end_edge)
+                                N_global[end_edge] = ""
+                                return
+                            if e in N_path:
+                            # will find wrong path
+                                print("find wrong!")
+                                return
 
         for next_v in sort_path(end_vertex, next_v_list):
             weight_1 = weight + G[end_vertex][next_v]['weight']
             if weight_1 < max_weight:  # weight overflow
+                next_edge = (end_vertex, next_v)
+
+                if next_edge in Y:  # check early cycle
+                    for e in Y[next_edge]:
+                        if (e in N_global) or (e in N_path):  # WA
+                            continue
+
                 if next_v is t:  # reach sink, finish
                     if check_path(path + [next_v]):
                         if verbose:
-                            print(max_weight, weight_1, i_searched, time.time()-t0)
+                            print(max_weight, weight_1,
+                                  i_searched, time.time() - t0)
                         max_weight = weight_1
                         num_paths += 1
                         valid_paths.append(path + [next_v])
+                    else:  # add DP information if not AC
+                        continue
+                        for i in range(len(path)):
+                            if i not in cycle:
+                                v = path[i]
+                                num_v_in_v1 = num_ele_in_dict(path[i:], dict_v1)
+                                if v in found_path:
+                                    if num_v_in_v1 > found_path[v]:
+                                        found_path[v] = num_v_in_v1
+                                else:
+                                    found_path[v] = num_v_in_v1
 
-                elif next_v not in path:  # new vertex
+                elif next_v in path:  # next_v in the path: forming cycle
+                    for vertex in path:
+                        cycle[vertex] = True
+                    cycle[next_v] = True
+
+                else:  # new vertex in the path, add to stack
                     next_edge = (end_vertex, next_v)
+                    # if next edge in N_global and N_path, continue
+                    if next_edge in N_global:
+                        continue
+                    if next_edge in N_path:  # wrong edge: forming cycle
+                        add_cycle(path + [next_v], cycle)
+                        continue
+
+                    # merge next edge to path Y/N dict
                     flag_Y = False
                     flag_N = False
-
                     if next_edge in Y:  # Y_path merge Y[next_edge]
                         flag_Y = True
                         Y_path1 = merge_dicts(Y_path, Y[next_edge])
@@ -192,19 +250,13 @@ def trevize(G, s, t, v1, verbose):
                         flag_N = True
                         N_path1 = merge_dicts(N_path, N[next_edge])
 
-                    # if next edge in N_global and N_path, continue
-                    if next_edge in N_global:
-                        continue
-                    if next_edge in N_path:  # wrong edge
-                        continue
-
                     i_searched += 1  # add to stack
                     if flag_Y and flag_N:
                         paths.appendleft([path + [next_v], weight_1,
                                          Y_path1, N_path1])
                     elif flag_Y:
                         paths.appendleft([path + [next_v], weight_1,
-                                         Y_path1, N_path])
+                                         Y_path1, N_path1])
                     elif flag_N:
                         paths.appendleft([path + [next_v], weight_1,
                                          Y_path, N_path1])
@@ -212,11 +264,13 @@ def trevize(G, s, t, v1, verbose):
                         paths.appendleft([path + [next_v], weight_1,
                                          Y_path, N_path])
 
+
     def merge_dicts(x, y):
         """Given two dicts, merge them into a new dict as a shallow copy."""
-        z = x.copy()
-        z.update(y)
-        return z
+        # z = x.copy()
+        # z.update(y)
+        # return z  u# for py < 3.5
+        return {**x, **y}
 
     def check_path(path):
         """Check if path contains all vertices in v1."""
@@ -235,12 +289,15 @@ def trevize(G, s, t, v1, verbose):
         """
 
         weight_list = {}
+        if len(next_list) is 1:
+            return list(next_list)
+
         for next_v in next_list:
             # generate value list: add large num to vertices not in v1
-            LARGE_NUM = 21
+            LARGE_NUM = 20
             if next_v is t:
                 weight_list[next_v] = 0
-            if next_v in v1:
+            if next_v in dict_v1:
                 weight_list[next_v] = next_list[next_v]['weight']
                 if iter_depth is 0:
                     nn_v_list = G[next_v]
@@ -265,6 +322,20 @@ def trevize(G, s, t, v1, verbose):
 
         return sorted(weight_list, key=weight_list.get)
 
+    def num_ele_in_dict(elements, d):
+        """How many elements are in the dict."""
+        num = 0
+        for i in elements:
+            if i in d:
+                num += 1
+        return num
+
+    def add_cycle(path, d_add):
+        """Add vertices in path dict d_add."""
+        for v in path:
+            d_add[v] = True
+        return
+
     init_N_list(G, N)
     paths.appendleft([[s], 0, {}, {}])
 
@@ -274,11 +345,17 @@ def trevize(G, s, t, v1, verbose):
     if verbose:  # verbose printout
         print("added route:", i_searched)
         print("num of paths: {}".format(num_paths))
+        print("Y_edge_pair:")
+        pprint(Y)
         print("N_edge_pair:")
         pprint(N)
         print("N_global:")
         pprint(N_global)
         pprint(valid_paths)
+        print("found_path:")
+        print(found_path)
+        print("cycle:")
+        pprint(cycle)
 
     if valid_paths:  # output
         return valid_paths, G
@@ -362,3 +439,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    # import cProfile
+    # cProfile.run('main()', 'restats')
+
+    # import pstats
+    # p = pstats.Stats('restats')
+    # p.sort_stats('cumulative').print_stats(20)
